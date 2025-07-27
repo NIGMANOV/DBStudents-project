7158d90a9bb0578354a6f449cf93a316d98101e0
NIGMANOV <masalan2001@gmail.com>
2025-07-27 13:46:15 +0500

    completed work with the user model, controller, route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,31 +2,146 @@ import User from "../models/user.model.js";
 import generateOTP from "../utils/otp.util.js";
 
 class UserControllers {
+  // Функция отвечает за создания нового пользователя
   async create(req, res) {
     try {
       const { first_name, last_name, email, password } = req.body;
 
       const candidate = await User.findOne({ where: { email } });
-      const otpCode = generateOTP();
+
       if (candidate) {
-        res
-          .status(404)
+        return res
+          .status(409)
           .json({ message: "Пользователь с таким email уже существует" });
       }
 
+      const otpCode = generateOTP();
+
       const userFormData = {
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        password: password,
+        first_name,
+        last_name,
+        email,
+        password,
         otp: otpCode,
       };
 
       const newUser = await User.create(userFormData);
 
-      res.status(201).json({ message: "Пользователь успешно создан", newUser });
+      res.status(201).json({
+        message: "Пользователь успешно создан",
+        user: newUser,
+      });
     } catch (error) {
-      res.status(404).json({ message: "Ошибка в создании пользователя" });
+      res.status(500).json({ message: "Ошибка в создании пользователя" });
+    }
+  }
+
+  // Функция отвечает за верификацию пользователя
+  async verified(req, res) {
+    try {
+      const { email, otp } = req.body;
+
+      const candidate = await User.findOne({ where: { email } });
+
+      if (!candidate || otp !== candidate.otp) {
+        return res.status(409).json({ message: "OTP код не верный" });
+      }
+
+      // Обновляем статус и сохраняем
+      candidate.is_verified = true;
+      candidate.otp = null; // (опционально) обнуляем OTP после верификации
+      await candidate.save(); // сохраняем в БД
+
+      res.status(200).json({
+        message: "Пользователь успешно верифицирован",
+        user: candidate,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Ошибка при верификации пользователя" });
+    }
+  }
+
+  // // Функция отвечает за авторизацию пользователя
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      const candidate = await User.findOne({ where: { email } });
+
+      if (!candidate) {
+        return res.status(409).json({ message: "Пароль или email не верны" });
+      }
+
+      if (candidate.password !== password) {
+        return res.status(409).json({ message: "Пароль или email не верны" });
+      }
+
+      if (!candidate.is_verified) {
+        return res.status(409).json({
+          message: "Пользователь не прошел верификацию, проверьте почту на OTP",
+        });
+      }
+
+      res.status(200).json({
+        message: "Пользователь успешно авторизовался",
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Ошибка в авторизации пользователя" });
+    }
+  }
+
+  // Функция отвечает за получения всех пользователей
+  async getAll(req, res) {
+    try {
+      const getAllUsers = await User.findAll();
+      res.status(200).json(getAllUsers);
+    } catch (error) {
+      res.status(500).json({ message: "Ошибка при получении пользователей" });
+    }
+  }
+
+  // Функция отвечает за получение пользователя по ID
+  async getById(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(404).json({ message: "Пользователь не найден" });
+      }
+
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Ошибка при получении пользователя" });
+    }
+  }
+
+  // Функция отвечает за удаление пользователя по ID
+  async deleteById(req, res) {
+    try {
+      const { id } = req.params;
+      const deletedCount = await User.destroy({ where: { user_id: id } });
+
+      if (deletedCount === 0) {
+        return res.status(404).json({ message: "Пользователь не найден" });
+      }
+
+      res.status(200).json({ message: "Пользователь успешно удалён" });
+    } catch (error) {
+      res.status(500).json({ message: "Ошибка при удалении пользователя" });
+    }
+  }
+
+  // Функция отвечает за удаление всех пользователей
+  async deleteAll(req, res) {
+    try {
+      await User.destroy({ where: {}, truncate: true });
+
+      res.status(200).json({ message: "Все пользователи были удалены" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Ошибка при удалении всех пользователей" });
     }
   }
 }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,5 +3,12 @@ import UserControllers from "../controllers/user.controller.js";
 const routerUser = express.Router();
 
 routerUser.post("/", UserControllers.create);
+routerUser.post("/otp", UserControllers.verified);
+routerUser.post("/login", UserControllers.login);
+routerUser.get("/", UserControllers.getAll);
+routerUser.get("/users/:id", UserControllers.getById);
+routerUser.delete("/users/:id", UserControllers.deleteById);
+routerUser.delete("/users", UserControllers.deleteAll);
+
 
 export default routerUser;
