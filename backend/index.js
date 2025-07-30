0f19edc2409df62253ffea9847172130bcad316f
NIGMANOV <masalan2001@gmail.com>
2025-07-30 20:13:13 +0500

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import sequelize from "./config/sequelize.config.js";
 import User from "./models/user.model.js";
@@ -9,6 +10,14 @@ import routerStudent from "./routes/student.route.js";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/api/user", routerUser);
 app.use("/api/student", routerStudent);
